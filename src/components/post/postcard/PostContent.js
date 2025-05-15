701801b2912bb6a28cb9358d9e554ed5aa5affbf
h91531 <h91531@naver.com>
2025-05-15 18:21:33 +0900

    '123'

--- src/components/post/postcard/PostContent.js
+++ src/components/post/postcard/PostContent.js
@@ -1,4 +1,5 @@
 import formatDate from "../../../utils/formatDate";
+import { useState } from "react";
 
 export default function PostContent({
   post,
@@ -9,10 +10,20 @@ export default function PostContent({
   setIsExpanded,
   onCommentClick
 }) {
+  const [isPushed, setIsPushed] = useState(false);
+    const handleClick = () => {
+    setIsPushed(prev => !prev); // 이전 상태 반전 (토글)
+  };
   return (
     <div className="txt_wrap">
-      <i className="comment_icon" onClick={onCommentClick}>
-        <img src="/comment.svg" alt="댓글 아이콘" /> {commentsLength}
+      <i className="comment_icon">
+    <img
+      src={isPushed ? "/push_heart.png" : "/heart.png"}
+      alt="좋아요"
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    />
+        <img src="/comment.svg" alt="댓글 아이콘" onClick={onCommentClick}/> {commentsLength}
       </i>
       <h2>{post?.title || "제목 없음"}</h2>
       <p
